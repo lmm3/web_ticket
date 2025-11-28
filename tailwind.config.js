16f7d1cdb8603a81a8335a96ddd5cd63ad0e9ee8
lmm3 <lmm3@cin.ufpe.br>
2025-11-28 10:10:10 -0300

    fix: added safelist for colour classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,10 @@ export default {
     },
     plugins: [],
   }
+
+  module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  safelist: [
+    { pattern: /(bg|hover:bg|text|border)-(blue|red|green|yellow)-(100|200|800)/ }
+  ]
+}
